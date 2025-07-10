4bf19eef557308746ce06496eeef96a59e1e51ee
Douglas Militao <96530706+DMilitao@users.noreply.github.com>
2025-07-10 14:47:26 -0300

    Delete build directory

--- build/src/CMakeFiles/ping_project.dir/icmp.cc.o.d
+++
@@ -1,4 +0,0 @@
-src/CMakeFiles/ping_project.dir/icmp.cc.o: \
- /home/douglas.pinheiro.lds/projetos/ping_proj/src/icmp.cc \
- /usr/include/stdc-predef.h \
- /home/douglas.pinheiro.lds/projetos/ping_proj/include/icmp.h
