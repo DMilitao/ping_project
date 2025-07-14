b5a37c149a79cebffb43a236312c9131cf9dfc05
Douglas Pinheiro <douglas.pinheiro.lds@datacom.com.br>
2025-07-14 13:10:29 -0300

    Improve and associate similiar methods

--- src/icmp.cc
+++ src/icmp.cc
@@ -19,96 +19,50 @@ bool icmp::Decode(std::vector<uint8_t> buffer){
      set_Type(buffer[0]);
      set_Code(buffer[1]);
 
-     if ( isEchoRequest() ){
-        std::cout << "Is Echo Request" << std::endl;
+     if ( isEchoRequest() || isEchoReply() || isInfoRequest() || isInfoReply() ){
 
-        uint16_t identifier = ((buffer[4] << 8) & 0xFF00) + (buffer[5] & 0xFF);
+        uint16_t identifier = (buffer[4] << 8) + buffer[5];
         set_Identifier(identifier);
 
-        uint16_t sequence_number = ((buffer[6] << 8) & 0xFF00) + (buffer[7] & 0xFF);
+        uint16_t sequence_number = (buffer[6] << 8)  + buffer[7];
         set_SequenceNumber(sequence_number);
 
      } else if ( isEchoReply() ) {
-        std::cout << "Is Echo Reply" << std::endl;
 
-        uint16_t identifier = ((buffer[4] << 8) & 0xFF00) + (buffer[5] & 0xFF);
+        uint16_t identifier = (buffer[4] << 8) + buffer[5];
         set_Identifier(identifier);
 
-        uint16_t sequence_number = ((buffer[6] << 8) & 0xFF00) + (buffer[7] & 0xFF);
+        uint16_t sequence_number = (buffer[6] << 8)  + buffer[7];
         set_SequenceNumber(sequence_number);
         
-     } else if ( isTimeStamp() ) {
-        std::cout << "Is TimeStamp" << std::endl;
+     } else if ( isTimeStamp() || isTimeStampReply() ) {
 
-        uint16_t identifier = ((buffer[4] << 8) & 0xFF00) + (buffer[5] & 0xFF);
+        uint16_t identifier = (buffer[4] << 8) + buffer[5];
         set_Identifier(identifier);
 
-        uint16_t sequence_number = ((buffer[6] << 8) & 0xFF00) + (buffer[7] & 0xFF);
+        uint16_t sequence_number = (buffer[6] << 8)  + buffer[7];
         set_SequenceNumber(sequence_number);
 
-        uint32_t originate_timestamp = ((buffer[8] << 24) & 0xFF000000) + ((buffer[9] << 16) & 0xFF0000) + ((buffer[10] << 8) & 0xFF00) + (buffer[11] & 0xFF);
+        uint32_t originate_timestamp = (buffer[8] << 24) + (buffer[9] << 16) + (buffer[10] << 8) + buffer[11];
         set_OriginateTimeStamp(originate_timestamp);
 
-        uint32_t receive_timestamp = ((buffer[12] << 24) & 0xFF000000) + ((buffer[13] << 16) & 0xFF0000) + ((buffer[14] << 8) & 0xFF00) + (buffer[15] & 0xFF);
+        uint32_t receive_timestamp = (buffer[12] << 24) + (buffer[13] << 16) + (buffer[14] << 8) + buffer[15];
         set_ReceiveTimeStamp(receive_timestamp);
 
-        uint32_t transmit_timestamp = ((buffer[16] << 24) & 0xFF000000) + ((buffer[17] << 16) & 0xFF0000) + ((buffer[18] << 8) & 0xFF00) + (buffer[19] & 0xFF);
+        uint32_t transmit_timestamp = (buffer[16] << 24)+ (buffer[17] << 16) + (buffer[18] << 8) + buffer[19];
         set_TransmitTimeStamp(transmit_timestamp);
 
         return true;
+     } else if ( isDestinationUnreach() || isSourceQuench() || isTimeExceed() ) {
 
-     } else if ( isTimeStampReply() ) {
-        std::cout << "Is TimeStamp Reply" << std::endl;
-
-        uint16_t identifier = ((buffer[4] << 8) & 0xFF00) + (buffer[5] & 0xFF);
-        set_Identifier(identifier);
-
-        uint16_t sequence_number = ((buffer[6] << 8) & 0xFF00) + (buffer[7] & 0xFF);
-        set_SequenceNumber(sequence_number);
-
-        uint32_t originate_timestamp = ((buffer[8] << 24) & 0xFF000000) + ((buffer[9] << 16) & 0xFF0000) + ((buffer[10] << 8) & 0xFF00) + (buffer[11] & 0xFF);
-        set_OriginateTimeStamp(originate_timestamp);
-
-        uint32_t receive_timestamp = ((buffer[12] << 24) & 0xFF000000) + ((buffer[13] << 16) & 0xFF0000) + ((buffer[14] << 8) & 0xFF00) + (buffer[15] & 0xFF);
-        set_ReceiveTimeStamp(receive_timestamp);
-
-        uint32_t transmit_timestamp = ((buffer[16] << 24) & 0xFF000000) + ((buffer[17] << 16) & 0xFF0000) + ((buffer[18] << 8) & 0xFF00) + (buffer[19] & 0xFF);
-        set_TransmitTimeStamp(transmit_timestamp);
-
-        return true;
-
-     } else if ( isInfoRequest() ) {
-        std::cout << "Is Info Request" << std::endl;
-      
-        uint16_t identifier = ((buffer[4] << 8) & 0xFF00) + (buffer[5] & 0xFF);
-        set_Identifier(identifier);
-
-        uint16_t sequence_number = ((buffer[6] << 8) & 0xFF00) + (buffer[7] & 0xFF);
-        set_SequenceNumber(sequence_number);
-     } else if ( isInfoReply() ) {
-        std::cout << "Is Info Reply" << std::endl;
- 
-        uint16_t identifier = ((buffer[4] << 8) & 0xFF00) + (buffer[5] & 0xFF);
-        set_Identifier(identifier);
-
-        uint16_t sequence_number = ((buffer[6] << 8) & 0xFF00) + (buffer[7] & 0xFF);
-        set_SequenceNumber(sequence_number);
-
-     } else if ( isDestinationUnreach() ) {
-        std::cout << "Is Dest Unreach" << std::endl;
      } else if ( isRedirectMessage() ) {
-        std::cout << "Is Redirect Message" << std::endl;
-        uint32_t gateway_address = ((buffer[4] << 24) & 0xFF000000) + ((buffer[5] << 16) & 0xFF0000) + ((buffer[6] << 8) & 0xFF00) + (buffer[7] & 0xFF);
+        uint32_t gateway_address = (buffer[4] << 24)+ (buffer[5] << 16) + (buffer[6] << 8)  + buffer[7];
         set_GatewayAddress(gateway_address);
-     } else if ( isSourceQuench() ) {
-        std::cout << "Is Source Quench" << std::endl;
-     } else if ( isTimeExceed()) {
-        std::cout << "Is Time Exceed" << std::endl;
+
      } else if ( isParameterProblem() ) {
-        std::cout << "Is Parameter Problem" << std::endl;
         set_Pointer(buffer[4]);
      } else {
-        std::cout << "Is None" << std::endl;
+      
         return false;
      }
      
@@ -125,8 +79,7 @@ std::vector<uint8_t> icmp::Encode(){
     msg.push_back(0);
     msg.push_back(0);
     
-    if ( isEchoRequest() ){
-        std::cout << "Is Echo Request" << std::endl;
+    if ( isEchoRequest() || isEchoReply() || isInfoRequest() || isInfoReply() ){
         uint8_t identifier_high = (identifier_ >> 8) & 0xFF;
         uint8_t identifier_low = (identifier_) & 0xFF;
         msg.push_back(identifier_high);
@@ -137,21 +90,7 @@ std::vector<uint8_t> icmp::Encode(){
         msg.push_back(sequence_number_high);
         msg.push_back(sequence_number_low);
 
-     } else if ( isEchoReply() ) {
-        std::cout << "Is Echo Reply" << std::endl;
-
-        uint8_t identifier_high = (identifier_ >> 8) & 0xFF;
-        uint8_t identifier_low = (identifier_) & 0xFF;
-        msg.push_back(identifier_high);
-        msg.push_back(identifier_low);
-
-        uint8_t sequence_number_high = (sequence_number_ >> 8) & 0xFF;
-        uint8_t sequence_number_low = (sequence_number_) & 0xFF;
-        msg.push_back(sequence_number_high);
-        msg.push_back(sequence_number_low);
-
-     } else if ( isTimeStamp() ) {
-        std::cout << "Is TimeStamp" << std::endl;
+     } else if ( isTimeStamp() ||  isTimeStampReply() ) {
         
         uint8_t identifier_high = (identifier_ >> 8) & 0xFF;
         uint8_t identifier_low = (identifier_) & 0xFF;
@@ -190,81 +129,9 @@ std::vector<uint8_t> icmp::Encode(){
         msg.push_back(transmit_timestamp_third);
         msg.push_back(transmit_timestamp_last);
         
-     } else if ( isTimeStampReply() ) {
-        std::cout << "Is TimeStamp Reply" << std::endl;
-
-        uint8_t identifier_high = (identifier_ >> 8) & 0xFF;
-        uint8_t identifier_low = (identifier_) & 0xFF;
-        msg.push_back(identifier_high);
-        msg.push_back(identifier_low);
-
-        uint8_t sequence_number_high = (sequence_number_ >> 8) & 0xFF;
-        uint8_t sequence_number_low = (sequence_number_) & 0xFF;
-        msg.push_back(sequence_number_high);
-        msg.push_back(sequence_number_low);
-
-        uint8_t originate_timestamp_first = (originate_timestamp_ >> 24) & 0xFF;
-        uint8_t originate_timestamp_second = (originate_timestamp_ >> 16) & 0xFF;
-        uint8_t originate_timestamp_third = (originate_timestamp_ >> 8) & 0xFF;
-        uint8_t originate_timestamp_last = (originate_timestamp_) & 0xFF;
-        msg.push_back(originate_timestamp_first);
-        msg.push_back(originate_timestamp_second);
-        msg.push_back(originate_timestamp_third);
-        msg.push_back(originate_timestamp_last);
-
-        uint8_t receive_timestamp_first = (receive_timestamp_ >> 24) & 0xFF;
-        uint8_t receive_timestamp_second = (receive_timestamp_ >> 16) & 0xFF;
-        uint8_t receive_timestamp_third = (receive_timestamp_ >> 8) & 0xFF;
-        uint8_t receive_timestamp_last = (receive_timestamp_) & 0xFF;
-        msg.push_back(receive_timestamp_first);
-        msg.push_back(receive_timestamp_second);
-        msg.push_back(receive_timestamp_third);
-        msg.push_back(receive_timestamp_last);
-
-        uint8_t transmit_timestamp_first = (transmit_timestamp_ >> 24) & 0xFF;
-        uint8_t transmit_timestamp_second = (transmit_timestamp_ >> 16) & 0xFF;
-        uint8_t transmit_timestamp_third = (transmit_timestamp_ >> 8) & 0xFF;
-        uint8_t transmit_timestamp_last = (transmit_timestamp_) & 0xFF;
-        msg.push_back(transmit_timestamp_first);
-        msg.push_back(transmit_timestamp_second);
-        msg.push_back(transmit_timestamp_third);
-        msg.push_back(transmit_timestamp_last);
-        
-     } else if ( isInfoRequest() ) {
-        std::cout << "Is Info Request" << std::endl;
-
-        uint8_t identifier_high = (identifier_ >> 8) & 0xFF;
-        uint8_t identifier_low = (identifier_) & 0xFF;
-        msg.push_back(identifier_high);
-        msg.push_back(identifier_low);
-
-        uint8_t sequence_number_high = (sequence_number_ >> 8) & 0xFF;
-        uint8_t sequence_number_low = (sequence_number_) & 0xFF;
-        msg.push_back(sequence_number_high);
-        msg.push_back(sequence_number_low);
-        
-     } else if ( isInfoReply() ) {
-        std::cout << "Is Info Reply" << std::endl;
-        
-        uint8_t identifier_high = (identifier_ >> 8) & 0xFF;
-        uint8_t identifier_low = (identifier_) & 0xFF;
-        msg.push_back(identifier_high);
-        msg.push_back(identifier_low);
-
-        uint8_t sequence_number_high = (sequence_number_ >> 8) & 0xFF;
-        uint8_t sequence_number_low = (sequence_number_) & 0xFF;
-        msg.push_back(sequence_number_high);
-        msg.push_back(sequence_number_low);
-        
-     } else if ( isDestinationUnreach() ) {
-        std::cout << "Is Dest Unreach" << std::endl;   
-        msg.push_back(0);
-        msg.push_back(0);
-        msg.push_back(0);
-        msg.push_back(0);
+     } else if ( isDestinationUnreach() || isSourceQuench() || isTimeExceed() ) {
         
      } else if ( isRedirectMessage() ) {
-        std::cout << "Is Redirect Message" << std::endl;
         uint8_t g_address_first = (gateway_address_ >> 24) & 0xFF;
         uint8_t g_address_second = (gateway_address_ >> 16) & 0xFF;
         uint8_t g_address_third = (gateway_address_ >> 8) & 0xFF;
@@ -274,31 +141,18 @@ std::vector<uint8_t> icmp::Encode(){
         msg.push_back(g_address_third);
         msg.push_back(g_address_last);
 
-     } else if ( isSourceQuench() ) {
-        std::cout << "Is Source Quench" << std::endl;
-        msg.push_back(0);
-        msg.push_back(0);
-        msg.push_back(0);
-        msg.push_back(0);
-
-     } else if ( isTimeExceed()) {
-        std::cout << "Is Time Exceed" << std::endl;
-        msg.push_back(0);
-        msg.push_back(0);
-        msg.push_back(0);
-        msg.push_back(0);
-        
      } else if ( isParameterProblem() ) {
-        std::cout << "Is Parameter Problem" << std::endl;
         msg.push_back(pointer_);
-        msg.push_back(0);
-        msg.push_back(0);
-        msg.push_back(0);
 
      } else {
         msg.clear();
         return msg;
      }
+
+      while ( msg.size() < 8 ) {
+         msg.push_back(0);
+      }
+
       msg.insert(msg.end(),data_.begin(),data_.end());
       clearCheckSum();
 
@@ -318,9 +172,9 @@ uint16_t icmp::createCheckSum(std::vector<uint8_t> buffer){
       uint32_t checksum = 0;
 
       for (std::size_t i = 0; i < buffer.size(); i += 2) {
-         checksum += ((buffer[i] << 8) & 0xFF00) + (buffer[i+1] & 0xFF);
+         checksum += (buffer[i] << 8) + buffer[i+1];
          
-         while ( checksum > 0x0000FFFF ) {
+         while ( checksum > 0xFFFF ) {
             checksum = ( checksum & 0xFFFF ) + ( ( checksum >> 16 ) & 0xFFFF );
          } 
       }
